f83231b3ac4be2a463f0762ea0dd24a85ec6a375
Landon Conover <landon.conover@uvu.edu>
2014-08-19 15:35:14 -0600

    All Done

--- reddit.js
+++ reddit.js
@@ -1,22 +1,79 @@
 #!/usr/bin/env node
 
-var request = require("request")
+var request = require("request");
+var prompt = require('prompt');
+var ASQ = require('asynquence');
+var exec = require('child_process').exec;
 
-var url = "http://www.reddit.com/r/climbing/.json"
+//first ask what subreddit you want
+ASQ().then(function(done,msg){
+    prompt.start();
 
-request({
-    url: url,
-    json: true
-}, function (error, response, body) {
+    prompt.get(['Subreddit'], function (err, result) {
+        if (err) { return onErr(err); }
+        console.log('Command-line input received:');
+        console.log(result['Subreddit']);
+        done(result['Subreddit']);
+      });
 
-    if (!error && response.statusCode === 200) {
+//next show the first 10 from that sub
+}).then(function(done,msg){
+    var url = "http://www.reddit.com/r/"+msg+"/.json"
 
-        // console.log(body.data.children);
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
 
-        for(var i = 0; i < body.data.children.length; i++) {
-            var obj = body.data.children[i];
+        if (!error && response.statusCode === 200) {
 
-            console.log(obj.data.title);
+            for(var i = 0; i < 10; i++) {
+                var obj = body.data.children[i];
+
+                console.log("#" + i + ". " + obj.data.title);
+                console.log("Up: "+obj.data.ups + " | Down: "+obj.data.downs + " | Score: "+obj.data.score);
+                console.log();
+                console.log("");
+            }
+            done(msg);
         }
-    }
+    })
+//ask the user if they want to see one
+}).then(function(done,msg){
+    prompt.start();
+
+    prompt.get(['Selection'], function (err, result) {
+        if (err) { return onErr(err); }
+        var data = {
+            sub: msg,
+            selection: result['Selection']
+        }
+
+        done(data);
+      });
+}).then(function(done,msg){
+    var url = "http://www.reddit.com/r/"+msg.sub+"/.json";
+
+
+
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            
+            for(var i = 0; i < 10; i++) {
+                var obj = body.data.children[i];
+
+                if (i==msg.selection) {
+                    exec('open '+obj.data.url, function (error, stdout, stderr) {
+                      // output is in stdout
+                    });
+                    console.log(obj.data.url);
+                };
+                
+            }
+        }
+    })
 })
