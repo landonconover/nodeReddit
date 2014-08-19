9b936ed8e5adf5a531904e98a759e53c2d6cf464
Landon Conover <landon.conover@uvu.edu>
2014-08-19 14:13:04 -0600

    Now Showing Titles

--- reddit.js
+++ reddit.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var request = require("request")
+
+var url = "http://www.reddit.com/r/climbing/.json"
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+        // console.log(body.data.children);
+
+        for(var i = 0; i < body.data.children.length; i++) {
+            var obj = body.data.children[i];
+
+            console.log(obj.data.title);
+        }
+    }
+})
